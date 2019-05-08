660dd7b52976a5f84433929a85c3ab02a2eadc9b
mini-militant <shailesh.coolcalm007@gmail.com>
2019-05-08 18:31:05 +0530

    3 files modified

--- components/Weather.js
+++ components/Weather.js
@@ -1,19 +1,31 @@
-import React from "react"
+import React from "react";
 
-class Weather extends React.Component{
-  render(){
-    return(
-        <div>
-          Location: {this.props.city}<br/>
-          Country :{this.props.country}<br/>
-          Temperature : {this.props.temperature}<br/>
-          Humidity: {this.props.humidity}<br/>
-          Description: {this.props.description}<br/>
-          {this.props.error && <p>{this.props.error}</p>} 
-          
-        </div>
-    );
-  }
-}
+const Weather = props => (
+	<div className="weather__info">
+	 {	
+	 	props.city && props.country && <p className="weather__key"> Location: 
+	 		<span className="weather__value"> { props.city }, { props.country }</span>
+	 	</p> 
+	 }
+	 { 	
+	 	props.temperature && <p className="weather__key"> Temperature: 
+	 		<span className="weather__value"> { props.temperature }	</span>
+	 	</p> 
+	 }
+	 { 	
+	 	props.humidity && <p className="weather__key"> Humidity: 
+	 		<span className="weather__value"> { props.humidity } </span>
+	 	</p> 
+	 }
+	 { 	
+	 	props.description && <p className="weather__key"> Conditions: 
+	 		<span className="weather__value"> { props.description } </span>
+	 </p> 
+	 }
+	 { 
+	 	props.error && <p className="weather__error">{ props.error }</p>  
+	 }
+	</div>
+);
 
 export default Weather;
